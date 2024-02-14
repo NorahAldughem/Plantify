ca47ddff7a4a39a623c3bc68407ae1b6d02095b2
NorahAldughem <147074212+NorahAldughem@users.noreply.github.com>
2024-02-14 16:37:24 +0300

    Initial Commit

---
+++ Plantify/PlantifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantifyApp.swift
+//  Plantify
+//
+//  Created by Norah Aldughem on 04/08/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct PlantifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
