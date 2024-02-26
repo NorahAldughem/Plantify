fbff32bd258cdef3647ffcc020b94f37a80e295c
NorahAldughem <147074212+NorahAldughem@users.noreply.github.com>
2024-02-26 15:54:06 +0300

    lll

--- Plantify/ContentView.swift
+++ Plantify/ContentView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     @State var segmentedChoice = 0
+    @State var checkCards : [CheckCard] = []
+    @State var PlantCards : [PlantCard] = []
+    @State var isshowingsheet = false
     
     var body: some View {
         NavigationStack{
@@ -37,7 +40,74 @@ struct ContentView: View {
                         
                     }
                     
-                    if (var plantCardViewModel: PlantCardViewModel)
+                    if (checkCards.isEmpty) {
+                        VStack{
+                            Image(systemName:"zzz")
+                                .font(.system(size: 32))
+                                .foregroundStyle(.icons)
+                                .padding()
+                            Text("You don't have anyplants that needs watering")
+                                .font(.caption)
+                                .foregroundStyle(.subtitleText)
+                                
+                        }
+                        .frame(width: 350, height: 270, alignment: .center)
+                        
+                    }
+                    else{
+                        ScrollView{
+                            VStack{
+                                ForEach (checkCards, id: \.self){
+                                  checkCard in
+                                    CheckCardView(CheckCard: checkCard)
+                                    
+                                }
+                            }
+                        }
+                    }
+                    HStack{
+                        Text("Office Plants")
+                            .font(.title2)
+                            .bold()
+                            .foregroundStyle(.titleText)
+                        Spacer()
+                        Button {
+                            isshowingsheet.toggle()
+                        } label: {
+                            Image(systemName: "plus")
+                                .font(.system(size: 24))
+                                .foregroundStyle(.backgroundButtons)
+                            
+                        }
+                        .sheet(isPresented: $isshowingsheet, content: {Sheet()
+                            
+                            
+                        })
+
+                    }
+                    if (PlantCards.isEmpty){
+                        VStack{
+                            Image(systemName: "leaf")
+                                .font(.system(size: 32))
+                                .foregroundStyle(.icons)
+                                .padding()
+                            Text("Tap ")
+                                .font(.system(size: 17))
+                                .foregroundStyle(.subtitleText)
+                            + Text(Image(systemName: "plus"))
+                                .font(.system(size: 22))
+                                .foregroundStyle(.backgroundButtons)
+                            + Text(" To dd one")
+                                .font(.system(size: 17))
+                                .foregroundColor(.subtitleText)
+                            
+                            Text("You can add all plants you have in your office")
+                                .font(.caption)
+                                .foregroundColor(.subtitleText)
+                            
+                        }
+                    }
+                        
 
                     
                 }
---
+++ Plantify/Sheet.swift
@@ -0,0 +1,18 @@
+//
+//  Sheet.swift
+//  Plantify
+//
+//  Created by Norah Aldughem on 15/08/1445 AH.
+//
+
+import SwiftUI
+
+struct Sheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Sheet()
+}
